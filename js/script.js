474dfce34331615fe96c0fa6f5ef1cf33f0267b5
salimsulaiman <salimsulaiman75@gmail.com>
2023-04-04 21:49:11 +0700

    add floating button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const changeNav = () => {
   var bodyElement = document.querySelector("body");
+  const floatingButton = document.getElementById("fl-btn");
   var navElement = document.querySelector("nav");
   //   this.scrollY > 50 ? (navElement.style.backgroundColor = "#fff") : (navElement.style.backgroundColor = "");
   if (this.scrollY > 20) {
@@ -8,6 +9,12 @@ const changeNav = () => {
   } else {
     navElement.style.backgroundColor = "";
     navElement.style.boxShadow = "";
+  }
+
+  if (this.scrollY > 700) {
+    floatingButton.style.display = "block";
+  } else {
+    floatingButton.style.display = "none";
   }
 };
 
